ac65a90e3bf1d7a8b4d39bad0cee6a59f6a24502
TM0D <tmodder24@gmail.com>
2021-04-05 16:50:08 -0500

    added autorun

--- scripts/first_install.sh
+++ scripts/first_install.sh
@@ -10,3 +10,5 @@ npm install rpi-led-control
 wget https://github.com/TM0D/ScoreSaberClock/releases/latest/download/ssclock.zip
 unzip ssclock.zip
 rm ssclock.zip
+sudo echo "./~/ssclock/scripts/run.sh &" > /etc/rc.local ## haha overwritng system files
+sudo echo "exit 0" > /etc/rc.local
